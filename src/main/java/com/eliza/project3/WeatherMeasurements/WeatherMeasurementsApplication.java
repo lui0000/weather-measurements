60f7b1f6ce5c900eb67aa2f24188020d0c6732ba
Lisa <elbondarenko04@gmail.com>
2024-10-15 16:12:56 +0300

    first commit

---
+++ src/main/java/com/eliza/project3/WeatherMeasurements/WeatherMeasurementsApplication.java
@@ -0,0 +1,13 @@
+package com.eliza.project3.WeatherMeasurements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMeasurementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMeasurementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliza/project3/WeatherMeasurements/WeatherMeasurementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliza.project3.WeatherMeasurements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMeasurementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
