93ef4020a7520ca9e5a2876606f42c124115af58
Lisa <elbondarenko04@gmail.com>
2024-10-15 19:01:09 +0300

    fill controller

--- src/main/java/com/eliza/project3/WeatherMeasurements/controllers/MeasurementController.java
+++ src/main/java/com/eliza/project3/WeatherMeasurements/controllers/MeasurementController.java
@@ -1,10 +1,67 @@
 package com.eliza.project3.WeatherMeasurements.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eliza.project3.WeatherMeasurements.dto.MeasurementDTO;
+import com.eliza.project3.WeatherMeasurements.models.Measurement;
+import com.eliza.project3.WeatherMeasurements.services.MeasurementService;
+import com.eliza.project3.WeatherMeasurements.util.MeasurementNotCreatedException;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/measurement")
 public class MeasurementController {
+    private final MeasurementService measurementService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public MeasurementController(MeasurementService measurementService, ModelMapper modelMapper) {
+        this.measurementService = measurementService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping
+    public List<MeasurementDTO> getPeople() {
+        return measurementService.findAll().stream().map(this::convertToMeasurementDTO).collect(Collectors.toList());
+    }
+    
+    @PostMapping
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid MeasurementDTO measurementDTO, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            StringBuilder errorMsg = new StringBuilder();
+            
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for( FieldError error : errors) {
+                errorMsg.append(error.getField()).append(" - ")
+                        .append(error.getDefaultMessage())
+                        .append(";");
+            }
+            throw new MeasurementNotCreatedException(errorMsg.toString());
+        }
+        measurementService.save(convertToMeasurement(measurementDTO));
+    }
+
+    private Measurement convertToMeasurement(MeasurementDTO measurementDTO) {
+        return modelMapper.map(measurementDTO, Measurement.class);
+    }
+
+
+    @GetMapping("/{id}")
+    public MeasurementDTO getMeasurement(@PathVariable("id") int id) {
+        //status 200
+        return convertToMeasurementDTO(measurementService.findOne(id));
+    }
+
+    private MeasurementDTO convertToMeasurementDTO(Measurement measurement) {
+        return modelMapper.map(measurement, MeasurementDTO.class);
+    }
 }
---
+++ src/main/java/com/eliza/project3/WeatherMeasurements/util/MeasurementNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.eliza.project3.WeatherMeasurements.util;
+
+public class MeasurementNotCreatedException extends RuntimeException{
+    public MeasurementNotCreatedException(String msg) {
+        super(msg);
+    }
+}
